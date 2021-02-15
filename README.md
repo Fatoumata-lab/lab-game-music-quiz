885d7bc8ca7fc79940d31475c21f411e3ca17de4
Fatoumata Diaby <fatoumata.diaby@outlook.com>
2021-02-15 10:29:58 +0100

    first push

---
+++ README.md
@@ -0,0 +1,1 @@
+# lab-game-music-quiz
---
+++ game.js
@@ -0,0 +1,40 @@
+// selection of all relevant tags
+
+const nextButton = document.getElementById("next-btn");
+const questionContainer = document.querySelector(".container");
+const questions = document.querySelector(".question");
+const audioTrack = document.querySelector(".audio");
+
+const questionsList = [
+]
+
+/*const q1 = {question: "Who interpreted 'Georgy Porgy'?", choice:["Choice 1","Choice2"], names:["Usher & Chilli", "Eric Bennet & Faith Evans"], photos:["",""]}
+const q2 = {question: "Who interpreted 'Changes'?", choice:["Choice 1","Choice2"], names:["Tupac", "Biggie"], photos:["",""]}
+const q3 = {question: "Who interpreted 'If your girl only know'?", choice:["Choice 1","Choice2"], names:["Aaliyah", "TLC"], photos:["",""]}
+const q4 = {question: "Who interpreted 'Family Affair'?", choice:["Choice 1","Choice2"], names:["Mary J Blidge", "Ashanti"], photos:["",""]}
+const q5 = {question: "Who interpreted 'U know what's up'?", choice:["Choice 1","Choice2"], names:["Donnell Jones", "112"], photos:["",""]}
+const q6 = {question: "Who interpreted 'Single ladies'?", choice:["Choice 1","Choice2"], names:["Beyonce", "Kelly Rowland"], photos:["",""]}
+const q7 = {question: "Who interpreted 'When I See U'?", choice:["Choice 1","Choice2"], names:["Mary J Blidge", "Fantasia"], photos:["",""]}
+const q8 = {question: "Who interpreted 'That's what I like'?", choice:["Choice 1","Choice2"], names:["Chris Brown", "Bruno Mars"], photos:["",""]}
+const q9 = {question: "Who interpreted 'Body Party'?", choice:["Choice 1","Choice2"], names:["Ciara", "Beyonce"], photos:["",""]}
+const q10 = {question: "Who interpreted 'You changed me'?", choice:["Choice 1","Choice2"], names:["Jamie Foxx & Chris Brown", "Ne-Yo"], photos:["",""]}
+
+const questions = ["q1","q2","q3","q4","q5","q6","q7","q8","q9","q10"]
+
+console.log(q1[0])
+*/
+
+
+nextButton.addEventListener('click', goToNextQuestion);
+
+function goToNextQuestion() {
+    
+}
+
+function selectAnswer() {
+
+}
+
+function totalPoints () {
+
+}
