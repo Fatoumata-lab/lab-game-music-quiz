3b9ff6eba11567be7ac86b40ffa290d17be6272d
Fatoumata Diaby <fatoumata.diaby@outlook.com>
2021-02-18 09:21:43 +0100

    to deploy

--- game.js
+++ game.js
@@ -13,25 +13,25 @@ const goodAnswer = document.querySelector(".good-answers");
 
 console.log(audioTrack)
 
-const q1 = {question: "1. Who interpreted 'Georgy Porgy'?", audio:"audio/Eric-Faith.mp3", answer: 1, names1:"Usher & Chilli", names2:"Eric Bennet & Faith Evans", photos1:"images/CU.jpg", photos2:"images/EricFaith.jpg", button: 'Next Question'}
+const q1 = {question: "1. Who interpreted 'Georgy Porgy'?", audio:"./audio/Eric-Faith.mp3", answer: 1, names1:"Usher & Chilli", names2:"Eric Bennet & Faith Evans", photos1:"./images/CU.jpg", photos2:"./images/EricFaith.jpg", button: 'Next Question'}
 
-const q2 = {question: "2. Who interpreted 'Changes'?", audio:"audio/2pac.mp3", answer: 0, names1:"Tupac", names2:"Biggie", photos1:"images/tupac.jpg", photos2:"images/Biggie.jpg", button: 'Next Question'}
+const q2 = {question: "2. Who interpreted 'Changes'?", audio:"./audio/2pac.mp3", answer: 0, names1:"Tupac", names2:"Biggie", photos1:"./images/tupac.jpg", photos2:"./images/Biggie.jpg", button: 'Next Question'}
 
-const q3 = {question: "3. Who interpreted 'If your girl only know'?", audio:"audio/Aaliyah.mp3", answer: 0, names1:"Aaliyah", names2:"TLC", photos1:"images/Aaliyah.jpg", photos2:"images/tlc.jpg", button: 'Next Question'}
+const q3 = {question: "3. Who interpreted 'If your girl only know'?", audio:"./audio/Aaliyah.mp3", answer: 0, names1:"Aaliyah", names2:"TLC", photos1:"./images/Aaliyah.jpg", photos2:"./images/tlc.jpg", button: 'Next Question'}
 
-const q4 = {question: "4. Who interpreted 'Family Affair'?", audio:"audio/Family-Affair.mp3", answer: 0, names1:"Mary J Blidge", names2:"Ashanti", photos1:"images/mary-j-blige.jpg", photos2:"images/ashanti.jpg", button: 'Next Question'}
+const q4 = {question: "4. Who interpreted 'Family Affair'?", audio:"./audio/Family-Affair.mp3", answer: 0, names1:"Mary J Blidge", names2:"Ashanti", photos1:"./images/mary-j-blige.jpg", photos2:"./images/ashanti.jpg", button: 'Next Question'}
 
-const q5 = {question: "5. Who interpreted 'U know what's up'?", audio:"audio/Donell-Jones.mp3", answer: 0, names1:"Donnell Jones", names2:"112", photos1:"images/Donell-Jones.jpg", photos2:"images/112.jpg", button: 'Next Question'}
+const q5 = {question: "5. Who interpreted 'U know what's up'?", audio:"./audio/Donell-Jones.mp3", answer: 0, names1:"Donnell Jones", names2:"112", photos1:"./images/Donell-Jones.jpg", photos2:"./images/112.jpg", button: 'Next Question'}
 
-const q6 = {question: "6. Who interpreted 'Single ladies'?", audio:"audio/Single-ladies.mp3", answer: 0, names1:"Beyonce", names2:"Kelly Rowland", photos1:"images/beyonce.png", photos2:"images/kelly-rowland.jpg", button: 'Next Question'}
+const q6 = {question: "6. Who interpreted 'Single ladies'?", audio:"./audio/Single-ladies.mp3", answer: 0, names1:"Beyonce", names2:"Kelly Rowland", photos1:"./images/beyonce.png", photos2:"./images/kelly-rowland.jpg", button: 'Next Question'}
 
-const q7 = {question: "7. Who interpreted 'When I See U'?", audio:"audio/Fantasia.mp3", answer: 1, names1:"Mary J Blidge", names2:"Fantasia", photos1:"images/mary-j-blige.jpg", photos2:"images/Fantasia.jpg", button: 'Next Question'}
+const q7 = {question: "7. Who interpreted 'When I See U'?", audio:"./audio/Fantasia.mp3", answer: 1, names1:"Mary J Blidge", names2:"Fantasia", photos1:"./images/mary-j-blige.jpg", photos2:"./images/Fantasia.jpg", button: 'Next Question'}
 
-const q8 = {question: "8. Who interpreted 'That's what I like'?", audio:"audio/Bruno-Mars.mp3", answer: 1, names1:"Chris Brown", names2:"Bruno Mars", photos1:"images/chris-brown.jpg", photos2:"images/bruno-mars.jpg", button: 'Next Question'}
+const q8 = {question: "8. Who interpreted 'That's what I like'?", audio:"./audio/Bruno-Mars.mp3", answer: 1, names1:"Chris Brown", names2:"Bruno Mars", photos1:"./images/chris-brown.jpg", photos2:"./images/bruno-mars.jpg", button: 'Next Question'}
 
-const q9 = {question: "9. Who interpreted 'Body Party'?", audio:"audio/Ciara.mp3", answer: 0, names1:"Ciara", names2:"Beyonce", photos1:"images/ciara.jpeg", photos2:"images/beyonce.png", button: 'Next Question'}
+const q9 = {question: "9. Who interpreted 'Body Party'?", audio:"./audio/Ciara.mp3", answer: 0, names1:"Ciara", names2:"Beyonce", photos1:"./images/ciara.jpeg", photos2:"./images/beyonce.png", button: 'Next Question'}
 
-const q10 = {question: "10. Who interpreted 'You changed me'?", audio:"audio/Jamie-Chris.mp3", answer: 0, names1:"Jamie Foxx & Chris Brown", names2:"Ne-Yo", photos1:"images/JamieChris.jpg", photos2:"images/neyo.jpg", button: 'Results'}
+const q10 = {question: "10. Who interpreted 'You changed me'?", audio:"./audio/Jamie-Chris.mp3", answer: 0, names1:"Jamie Foxx & Chris Brown", names2:"Ne-Yo", photos1:"./images/JamieChris.jpg", photos2:"./images/neyo.jpg", button: 'Results'}
 
 const questions = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
 
@@ -106,8 +106,7 @@ function handleClick() {
 
 //reset state function
 
-const div = document.getElementById("options");
-
+/*const div = document.getElementById("options");
 function resetState() {
     div.classList.add('hide')
-}
+}*/
